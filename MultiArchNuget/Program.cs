8806b4e4e3a777adb622f1c3123670ef7070a1d0
Calvin Wilkinson <kinsondigital@gmail.com>
2020-03-06 15:19:58 -0500

    Add project files.

---
+++ MultiArchNuget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiArchNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
