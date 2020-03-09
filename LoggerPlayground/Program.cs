e0c5221fca70996e26b6e39255c665577951733e
Calvin Wilkinson <kinsondigital@gmail.com>
2020-03-09 09:56:36 -0400

    WIP

---
+++ LoggerPlayground/Program.cs
@@ -0,0 +1,15 @@
+﻿using MultiArchNuget;
+using System;
+
+namespace LoggerPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("Hello World");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiArchNuget/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiArchNuget
+{
+    public static class Logger
+    {
+        private static string _prefix = "N/A";
+
+
+        public static void Log(string msg)
+        {
+#if DEBUG86
+            _prefix = "Debug-x86";
+#elif DEBUG64
+            _prefix = "Debug-x64";
+#elif RELEASE86
+            _prefix = "Release-x86";
+#elif RELEASE64
+            _prefix = "Release-x64";
+#endif
+
+            Console.WriteLine($"{_prefix}: {msg}");
+        }
+    }
+}
